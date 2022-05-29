cb80484b1983a9b4333fc2e921e617b8126136c3
itsjeremyhsieh <jeremy.life0107@gmail.com>
2022-05-29 11:57:45 +0800

    delete images

--- group_26/index.php
+++ group_26/index.php
@@ -261,40 +261,7 @@ mysqli_query($link, "SET collation_connection = 'utf8_unicode_ci'");
                 </div>
             </div><!-- Feature Section End -->
 
-            <!-- Brand Section Start -->
-            <div class="brand-section section section-padding pt-0">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="brand-slider">
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-1.png" alt="">
-                            </div>
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-2.png" alt="">
-                            </div>
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-3.png" alt="">
-                            </div>
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-4.png" alt="">
-                            </div>
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-5.png" alt="">
-                            </div>
-
-                            <div class="brand-item col">
-                                <img src="assets/images/brands/brand-6.png" alt="">
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div><!-- Brand Section End -->
+           
 
             <?php include "footer.php" ?>
 
